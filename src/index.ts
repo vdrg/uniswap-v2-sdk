972c0a424f94511a57d63f024dc044b39622e983
Vicente Dragicevic <vdrg@protonmail.ch>
2021-03-05 16:25:16 -0300

    Export Config interface

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import JSBI from 'jsbi'
 export { JSBI }
 
-export { BigintIsh, ChainId, TradeType, Rounding, MINIMUM_LIQUIDITY } from './constants'
+export { BigintIsh, ChainId, TradeType, Rounding, MINIMUM_LIQUIDITY, Config } from './constants'
 
 export * from './errors'
 export * from './entities'
